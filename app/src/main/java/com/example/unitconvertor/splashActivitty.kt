a3346d7ac214e1873cd182d78de6569b5c4734ac
vanshguptaji <vansh23164033@akgec.ac.in>
2024-10-27 21:54:01 +0530

    Splash Screen DOne

--- app/src/main/java/com/example/unitconvertor/splashActivitty.kt
+++ app/src/main/java/com/example/unitconvertor/splashActivitty.kt
@@ -1,23 +1,29 @@
 package com.example.unitconvertor
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
+class splashActivitty : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        val splashScreen = installSplashScreen()
-
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_activitty)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val iHome = Intent(this@splashActivitty, MainActivity::class.java)
+            startActivity(iHome)
+            finish() // Close SplashActivity after starting MainActivity
+        }, 1000)
     }
 }
-
