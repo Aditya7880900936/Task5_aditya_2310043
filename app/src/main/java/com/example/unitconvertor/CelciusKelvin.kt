98b3041f9f7869db636cee5fc1fb3be77cd6c90f
vanshguptaji <vansh23164033@akgec.ac.in>
2024-10-28 16:16:48 +0530

    Celcius to kelvin conversion page UI done

--- app/src/main/java/com/example/unitconvertor/CelciusKelvin.kt
+++ app/src/main/java/com/example/unitconvertor/CelciusKelvin.kt
@@ -1,31 +1,21 @@
 package com.example.unitconvertor
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CelciusKelvin : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//        val splashScreen = installSplashScreen()
-
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_celcius_kelvin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.CelciusKelvin)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val celciusKelvin : androidx.cardview.widget.CardView = findViewById<androidx.cardview.widget.CardView>(R.id.CelciusKelvin)
-
-        celciusKelvin.setOnClickListener{
-            val intent = Intent(this@MainActivity, CelciusKelvin::class.java)
-            startActivity(intent)
-        }
     }
 }
-
